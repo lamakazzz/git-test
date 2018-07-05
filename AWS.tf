4f975aa92c9acb170591a17dd94c40d246606a86
Yevhen_Nadin <Yevhen_Nadin@epam.com>
2018-07-05 18:17:14 +0300

    add tf-file

---
+++ AWS.tf
@@ -0,0 +1,20 @@
+/*
+* AWS connection.
+*/
+
+provider "aws" {
+  region = "us-west-2"
+}
+  
+resource "aws_instance" "test_VM" {
+  ami           = "ami-efd0428f"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "${var.vm_name}"
+  }
+}
+
+variable "vm_name" {
+  description = "Name for VM to be created"
+}
