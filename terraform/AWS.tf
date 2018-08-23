1a21687e468ac38b325f55d578824a51cc3fd454
Yevhen_Nadin <Yevhen_Nadin@epam.com>
2018-08-23 14:45:24 +0300

    test push

--- terraform/AWS.tf
+++ terraform/AWS.tf
@@ -12,8 +12,7 @@ resource "aws_instance" "test_VM" {
   instance_type = "t2.micro"
 
   tags {
-    Name = "8675675 ${var.vm_name}"
-    test1 = "test tag auto12-kjhkj"
+    Name = "${var.vm_name}"
   }
 }
 
